3838457078c4dd9388e21ccc0f3b5917734b9f5d
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2023-02-24 14:53:36 +0530

    j

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    kotlin("jvm") version "1.7.10"
+    id("java-gradle-plugin")
+    id("maven-publish")
+}
+
+group = "com.lagradost.cloudstream3"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+repositories {
+    mavenCentral()
+    google()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation("com.squareup.okhttp:okhttp:2.7.5")
+    implementation("org.jsoup:jsoup:1.15.3")
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
+    implementation("com.google.guava:guava:31.0.1-jre")
+
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // Use the Kotlin JUnit integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+
+    // This dependency is exported to consumers, that is to say found on their compile classpath.
+    api("org.apache.commons:commons-math3:3.6.1")
+}
